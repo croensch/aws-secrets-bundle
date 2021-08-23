4b31914d4333a35283930ce897a919ac0707e1db
Constup <16638173+constup@users.noreply.github.com>
2021-08-23 15:30:01 +0200

    Change bundle name to follow Symfony convention.

---
+++ src/ConstupAwsSecretsBundle.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Constup\AwsSecretsBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class ConstupAwsSecretsBundle extends Bundle
+{
+
+}
